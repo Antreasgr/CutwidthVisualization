8b71d83eaef039ca09f1ddafb399b509ebb733c4
Antreas gribas <antreasgribas@gmail.com>
2016-12-19 19:41:01 +0200

    refactoring

---
+++ bipartite.js
@@ -0,0 +1,120 @@
+function BipartiteGraph(graph) {
+    this.radius = 3;
+    this.margin = { x: 100, y: 20 };
+
+    this.diagonal = d3.svg.diagonal().projection(function (d) { return [d.x, d.y]; });
+
+    this.line = d3.svg.line().x(function (point) { return point.x; }).y(function (point) { return point.y; });
+
+    this.originalGraph = graph;
+
+    this.createBipartiteGraph = function () {
+        var nodes = {};
+        var nodesArray = [];
+        var links = [];
+
+        for (var i = 0; i < this.originalGraph.links.length; i++) {
+            var link = this.originalGraph.links[i];
+            var s = link.source;
+            var t = link.target;
+
+            if (!nodes[t.name]) {
+                nodes[t.name] = { name: t.name, sourceNode: t };
+                nodesArray.push(nodes[t.name]);
+            }
+
+            if (!nodes[s.name]) {
+                nodes[s.name] = { name: s.name, sourceNode: s };
+                nodesArray.push(nodes[s.name]);
+            }
+
+            for (var j = 0; j < s.size * t.size; j++) {
+                var extraNode = { name: s.name + "-" + t.name + "-" + j, sourceNode: null };
+                if (nodes[extraNode.name]) {
+                    // console.log(extraNode.name);
+                    // console.log(link);
+                }
+                nodes[extraNode.name] = extraNode;
+                nodesArray.push(nodes[extraNode.name]);
+
+                links.push({ source: nodes[s.name], target: extraNode });
+                links.push({ source: nodes[t.name], target: extraNode });
+            }
+        }
+
+        this.graph = { nodes: nodesArray, links: links };
+    }
+
+    this.drawGraph = function () {
+        var p = d3.select("#bipartite");
+
+        var yleft = 0, yright = 0;
+        for (var i = 0; i < this.graph.nodes.length; i++) {
+            if (this.graph.nodes[i].sourceNode) {
+                this.graph.nodes[i].x = 10;
+                this.graph.nodes[i].y = yleft;
+                yleft += this.margin.y;
+            }
+            else {
+                this.graph.nodes[i].x = 10 + this.margin.x;
+                this.graph.nodes[i].y = yright;
+                yright += this.margin.y;
+            }
+        }
+
+        // Create nodes
+        var dnodes = p.selectAll(".bp-node").data(this.graph.nodes, function (d) { return d.name; });
+
+
+        // node.transition().duration(500).attr("cx", function (d, i) { return d.sourceNode ? 10 : 50; })
+        //     .attr("cy", function (d, i) { d.sourceNode ? yleft +=ymargin : yright += ymargin; return d.sourceNode ? yleft: yright;  })
+
+        dnodes.enter()
+            .append("circle")
+            .attr("class", "bp-node")
+            .attr("id", function (d, i) { return d.name; })
+            .attr("cx", function (d, i) { return d.x })
+            .attr("cy", function (d, i) { return d.y })
+            .attr("r", this.radius)
+            .style("fill", function (d, i) { return d.sourceNode ? "#000" : "#fff" })
+            .style("stroke", function (d, i) { return d.sourceNode ? null : "#000" });
+
+        dnodes.exit().remove();
+
+        // Create links
+        // <path class="treelink" d="M278.70000000000005,100C278.70000000000005,150 232.25,150 232.25,200"></path>
+
+        var dlinks = p.selectAll("path.bp-link").data(this.graph.links);
+
+        // dlinks.transition().duration(500).attr("d", this.diagonal);
+
+        var that = this;
+        dlinks.enter().insert("path", "g")
+            .attr("class", "bp-link")
+            .transition().duration(500)
+            .attr("d", function (d) { return that.line([{ x: d.source.x, y: d.source.y }, { x: d.target.x, y: d.target.y }]); });
+
+        dlinks.exit().remove();
+    }
+
+    this.DynamicAlgorithm = function () {
+        // display 
+        var minDisplay = d3.select("#bipartite").selectAll(".maxcut").data(["Please wait"]);
+
+        minDisplay.enter().append("text").attr("id", "max-bp").attr("class", "maxcut")
+            .attr("x", window.innerWidth * 0.2 / 2).attr("y", -10)
+            .text(function (d) { return d; });
+
+        for (var i = 0; i < this.graph.nodes.length; i++) {
+            this.graph.nodes[i].size = 1;
+        }
+
+        var that = this;
+        setTimeout(function () {
+            var min = DynamicAlgorithm.DynamicCutWidth(that.graph, DynamicAlgorithm.fCutWidth);
+
+            var m = d3.select("#bipartite").selectAll(".maxcut").data([min.Value]);
+            m.text(function (d) { return d; });
+        });
+    }
+}
---
+++ dynamic-algorithm.js
@@ -0,0 +1,181 @@
+
+//Adding new Functionality to the skillet
+(function (DynamicAlgorithm) {
+
+    /**
+    * Functions for dynamic programming algorithm according to
+    * [A Note on exact Algorithms for Vertex Ordering 2011]
+    * http://users.uoa.gr/~sedthilk/papers/notexact.pdf
+    * The function returning the cutwidth of a specific order
+    **/
+    DynamicAlgorithm.fCutWidth = function (S, v, links) {
+        var newS = new Set(S);
+        var iS = new Set(S);
+        newS.add(v);
+        var cut = 0;
+        var left = 0, right = 0;
+        for (var i = 0; i < links.length; i++) {
+            if ((iS.has(links[i].source) && !newS.has(links[i].target)) ||
+                (!newS.has(links[i].source) && iS.has(links[i].target))) {
+
+                cut += links[i].source.size * links[i].target.size;
+            }
+
+            if (links[i].source == v) {
+                if (iS.has(links[i].target)) {
+                    left += links[i].target.size;
+                }
+                else {
+                    right += links[i].target.size;
+                }
+            }
+            else if (links[i].target == v) {
+                if (iS.has(links[i].source)) {
+                    left += links[i].source.size;
+                }
+                else {
+                    right += links[i].source.size;
+                }
+            }
+        }
+
+        var insideCut = 0;
+        for (var i = 0; i <= v.size; i++) {
+            insideCut = Math.max(insideCut, -(i * i) + i * (v.size + right - left) + v.size * left);
+        }
+
+        return cut + insideCut;
+    }
+
+    DynamicAlgorithm.DynamicCutWidth = function (graph, cutwidth_fn) {
+        var A = {};
+        var nodes = graph.nodes;
+        for (var i = 1; i <= nodes.length; i++) {
+            var comb = k_combinations(nodes, i);
+            for (var j = 0; j < comb.length; j++) {
+                var minW = { Value: Number.MAX_VALUE, Order: [] };
+                for (var w = 0; w < comb[j].length; w++) {
+                    // build the S \ {w}
+                    var smw = new Array();
+                    for (var k = 0; k < comb[j].length; k++) {
+                        if (k != w) {
+                            smw.push(comb[j][k]);
+                        }
+                    }
+
+                    var hashp = hashArray(smw);
+                    var f = Math.max(cutwidth_fn(smw, comb[j][w], graph.links), (A[hashp]) ? A[hashp].Value : 0);
+                    if (f < minW.Value) {
+                        minW.Value = f;
+                        if (A[hashp]) {
+                            minW.Order = A[hashp].Order.slice();
+                            minW.Order.push(comb[j][w]);
+                        }
+                        else {
+                            minW.Order = [comb[j][w]];
+                        }
+                    }
+                    //minW = Math.min(minW.Value, f);
+                }
+
+                A[hashArray(comb[j])] = minW;
+            }
+        }
+
+        for (var i = 0; i < A[hashArray(nodes)].Order.length; i++) {
+            A[hashArray(nodes)].Order[i].order = i;
+        }
+
+        return A[hashArray(nodes)];
+    }
+
+    //Helper function to hash a set of vertices
+    var hashArray = function (a) {
+        if (!(a instanceof Array))
+            return '';
+
+        var copy = a.slice();
+
+        copy.sort(function (a, b) {
+            if (a.name < b.name) return -1;
+            if (a.name > b.name) return 1;
+            return 0;
+        });
+
+        var hash = '';
+        for (var i = 0; i < copy.length; i++) {
+            hash += copy[i].name + ",";
+        }
+
+        return hash;
+    }
+
+    /**
+      * K-combinations
+      *
+      * Get k-sized combinations of elements in a set.
+      *
+      * Usage:
+      *   k_combinations(set, k)
+      *
+      * Parameters:
+      *   set: Array of objects of any type. They are treated as unique.
+      *   k: size of combinations to search for.
+      *
+      * Return:
+      *   Array of found combinations, size of a combination is k.
+      *
+      * Examples:
+      *
+      *   k_combinations([1, 2, 3], 1)
+      *   -> [[1], [2], [3]]
+      *
+      *   k_combinations([1, 2, 3], 2)
+      *   -> [[1,2], [1,3], [2, 3]
+      *
+      *   k_combinations([1, 2, 3], 3)
+      *   -> [[1, 2, 3]]
+      *
+      *   k_combinations([1, 2, 3], 4)
+      *   -> []
+      *
+      *   k_combinations([1, 2, 3], 0)
+      *   -> []
+      *
+      *   k_combinations([1, 2, 3], -1)
+      *   -> []
+      *
+      *   k_combinations([], 0)
+      *   -> []
+      */
+    var k_combinations = function (set, k) {
+        var i, j, combs, head, tailcombs;
+
+        if (k > set.length || k <= 0) {
+            return [];
+        }
+
+        if (k == set.length) {
+            return [set];
+        }
+
+        if (k == 1) {
+            combs = [];
+            for (i = 0; i < set.length; i++) {
+                combs.push([set[i]]);
+            }
+            return combs;
+        }
+
+        combs = [];
+        for (i = 0; i < set.length - k + 1; i++) {
+            head = set.slice(i, i + 1);
+            tailcombs = k_combinations(set.slice(i + 1), k - 1);
+            for (j = 0; j < tailcombs.length; j++) {
+                combs.push(head.concat(tailcombs[j]));
+            }
+        }
+        return combs;
+    }
+
+} (window.DynamicAlgorithm = window.DynamicAlgorithm || {}));
